36cb7f34320c386ebe376fcd4d5a70584eed33c6
lorrytruck <125468648+lorrytruck@users.noreply.github.com>
2023-03-20 04:21:21 +0700

    Update ex4.c

--- lab1/ex4.c
+++ lab1/ex4.c
@@ -11,7 +11,7 @@ void bubble_sort(int* array, int size)
 {
     int i, j;
     for (i = 0; i < size - 1; ++i) {
-        for (j = 0; j < size - i; ++j) {
+        for (j = 0; j < size - i - 1; ++j) {
             if (array[j] > array[j + 1]) {
                 swap(&array[j], &array[j + 1]);
             }
