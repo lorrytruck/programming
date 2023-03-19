72fb46e34b256185125be10a354e86900a812766
lorrytruck <125468648+lorrytruck@users.noreply.github.com>
2023-03-20 04:18:38 +0700

    Update ex1.c

--- lab1/ex1.c
+++ lab1/ex1.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-void init(int* arr, int n)
+
+int* init(int *arr, int n)
 {
-    arr = malloc(n * sizeof(int)); 
+    arr = malloc(n * sizeof(int));
     int i;
     for (i = 0; i < n; ++i)
     {
         arr[i] = i;
     }
+    return arr;
 }
+
 int main()
 {
     int* arr = NULL;
     int n = 10;
-    init(arr, n);
+    arr = init(arr, n);
+
     int i;
     for (i = 0; i < n; ++i)
-    {
-        printf("%d\n", arr[i]);
-    }
+{
+    printf("%d\n", arr[i]);
+}
     return 0;
 }
