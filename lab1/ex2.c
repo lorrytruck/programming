b335b426f6b20c3a651788075217b89a472fd369
lorrytruck <reflect13off@gmail.com>
2023-03-19 23:13:41 +0700

    lab1

---
+++ lab1/ex2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stddef.h>
+typedef struct
+{
+ int num;
+ char str[3];
+} NumberRepr;
+void format(NumberRepr* number)
+{
+ sprintf(number->str, "%3d", number->num);
+}
+int main()
+{
+ NumberRepr number = { .num = 1025};
+ format(&number);
+ printf("str: %s\n", number.str);
+ printf("num: %d\n", number.num);
+ return 0;
+}
